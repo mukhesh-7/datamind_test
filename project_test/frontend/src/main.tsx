6b40664aeb6ffc6b41f2cd5c40975ef69a312d9f
Arumukhesh <arumukheshv@gmail.com>
2025-03-26 15:23:09 +0530

    Update main.tsx

--- project_test/frontend/src/main.tsx
+++ project_test/frontend/src/main.tsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.tsx';
 import './index.css';
 
-const CLIENT_ID = "1071489933676-kmm2c4h6i5rtoitg3vvl86ag4385vs3b.apps.googleusercontent.com";
+const CLIENT_ID = "{YOUR_CLIEND_ID}";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
